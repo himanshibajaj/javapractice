1383a01230cfbaef75a732f3dffe6149ca0ec124
Himanshi Bajaj <himanshibajaj723@gmail.com>
2024-10-12 23:49:41 +0530

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,8 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+    }
+}
